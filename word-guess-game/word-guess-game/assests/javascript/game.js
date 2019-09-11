7ddb4fd51e0c439bdc1a5e8ac668254225b7962b
jcpierce022 <54077239+jcpierce022@users.noreply.github.com>
2019-09-10 23:39:26 -0400

    Add files via upload

---
+++ word-guess-game/word-guess-game/assests/javascript/game.js
@@ -0,0 +1,122 @@
+game.js
+
+// define variables
+// var alpabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var answerArray = ["Schwarzenegger", "Dicaprio", "Bridges", "Eastwood", "Jackson"];
+var computerGuess = answerArray[Math.floor(Math.random() * answerArray.length)];
+var star = computerGuess.toLowerCase();
+var wins = 0;
+var count = 9;
+var guess = "";
+var hidden = setHidden();
+
+
+var winsText = document.getElementById("wins-text");
+var countText = document.getElementById("count-text");
+var guessesText = document.getElementById("guesses-text");
+var hiddenText = document.getElementById("hidden-text");
+
+
+winsText.textContent = "Wins: " + wins;
+countText.textContent = "Incorrect guesses left: " + count;
+guessesText.textContent = "Your guesses so far: " + guess;
+
+hiddenText.textContent = hidden;
+
+
+document.onkeyup = function (event) {
+    var userGuess = event.key.toLowerCase();
+
+   
+    var found = guess.search(userGuess);
+    if ((found === -1)) {
+       
+
+        guess = guess + " " + userGuess;
+
+        
+        var found1 = star.search(userGuess);
+        if ((found1 === -1)) {
+            count--;
+            // incorrect guess
+            if ((count === 0)) {
+                count = 9;
+                guess = "";
+                computerGuess = answerArray[Math.floor(Math.random() * answerArray.length)];
+                star = computerGuess.toLowerCase();
+                hidden = setHidden();
+                alert("You Lost :(");
+            }
+        }
+        else {
+            // correct guess
+            hidden = replaceHidden(found1, userGuess);
+            var counter = star.split(userGuess).length - 1;
+
+            // check for double letters ******
+            if (counter > 1) {
+                var last = found1 + 1;
+                for (var x = 0; x < counter - 1; x++) {
+
+                    var found3 = star.indexOf(userGuess, last);
+                    //alert("counter= " + counter +" f3= " + found3 +" f1= " + found1);
+                    hidden = replaceHidden(found3, userGuess);
+                    last = found3 + 1;
+                }
+            }
+
+            var winCheck = winner();
+            if (winCheck === 1) {
+                wins++;
+                count = 9;
+                guess = "";
+                computerGuess = answerArray[Math.floor(Math.random() * answerArray.length)];
+                star = computerGuess.toLowerCase();
+                hidden = setHidden();
+                alert("Congrats you won!");
+            }
+        }
+        // display to html
+        winsText.textContent = "Wins: " + wins;
+        countText.textContent = "Incorrect guesses left: " + count;
+        guessesText.textContent = "Your guesses so far: " + guess;
+        //ansText.textContent = "The answer is : " + computerGuess;
+        hiddenText.textContent = hidden;
+    }
+}
+
+function setHidden() {
+    var key = "";
+    for (var i = 0; i < star.length; i++) {
+        key = key + "_ ";
+    }
+    return key;
+}
+
+
+function replaceHidden(found1, userGuess) {
+    var key = "";
+    var index = 0;
+    if (found1 > 0) {
+        index = found1 + found1;
+    }
+    for (var i = 0; i < hidden.length; i++) {
+
+        var tempstr = hidden.substr(i, 1);
+        if (i == index) {
+            tempstr = userGuess;
+        }
+        //alert(userGuess + " f1=" +found1 +" "+ hidden.length + " " + i + " temp=" + tempstr);
+        key = key + tempstr;
+    }
+    return key;
+}
+
+
+function winner() {
+    var found = hidden.search("_");
+    if (found === -1) {
+        return 1;
+    }
+    return 0;
+}
